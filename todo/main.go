5c28e07c0ebf55b7f650ee9e19cf29e171745f0a
jschwartz <jadenmschwartz00@gmail.com>
2025-09-28 23:40:11 -0500

    todo list is good enought for now.

--- todo/main.go
+++ todo/main.go
@@ -2,40 +2,59 @@ package main
 
 import(
 	"fmt"
-	"os"
+	"bufio"
+    "os"
+	"strings"
 )
 
 func main(){
-	fmt.Println("Yes, this is yet another to do app written in go, there are many like it, but I did not use a tutorial to build this. Just my own raw brain power.")
-	fmt.Println("Yes, that also means it's likely not going to work very well.")
-	// we're going to choose to store our list in a "slice" in go. So I have heard on stack overflow, anytime we think of putting something in a list, we should use a slice.
-	// init the slice.
-	// i changed my mind let's make it an object of slices.
-	var todo_items []string // creates a string variable
-	todo_items = make([]string, 5) // sets it to a slice with length and capacity 5
-	var input string
-	type todo_list string; { // struct
-		var item_titles string
-	    var item_description string
-	    var item_duedate string
-}
-	fmt.Printf("%s\nSelect an action to take from below:\n")
-	fmt.Printf("%s  add (add an item to the list)\n")
-	fmt.Printf("%s  remove (remove an item from the list.) \n ")
-	fmt.Printf("%s  check (mark an item as complete.)\n")
-	fmt.Print("selector => ")
-	fmt.Scanln(&input)
-	fmt.Printf("%s", input)
-	if input == "add" {
-		fmt.Printf("%s adding item.\n")
-		fmt.Printf("%s\ntitle: ")
-		fmt.Scanln(&todo_list.item_titles)
-		fmt.Printf("%s\ndescription: ")
-		var item_description string
-		fmt.Scanln(&item_description)
-		fmt.Printf("%s\ndue date:")
-		var item_duedate string
-		fmt.Scanln(&item_duedate)
+	// there are better ways to store the to do list items, like for example, they could have due dates, and descriptions, titles etc.
+	// here, it is just a list with no due date, you can remove, add things etc. There is room to improve, but the overall idea
+	// of this project is just to build things as fast as possible, so here, there isn't much functionality, but perhaps in the future, that can change.
+	// start with a slice that represents the items in the todo list
+	var items_todo []string
+	items_todo = make([]string, 3)
 
+	keyboard := bufio.NewReader(os.Stdin)
+	fmt.Println("Basic ToDo List. Enter in one thing that you need to do to start.")
+	fmt.Print("Add thing to do => ")
+	text, _ := keyboard.ReadString('\n')
+	items_todo = append(items_todo, text)
+	fmt.Println("Starting To Do List >>", items_todo)
+
+	fmt.Println("What would you like to do next?")
+	var options []string
+	options = make([]string, 3)
+	options[0] = "add new item"
+	options[1] = "complete item"
+	options[2] = "print todo list"
+	fmt.Printf("%s\n%s\n%s\n", options[0], options[1], options[2])
+
+	fmt.Print("Choose an option =>")
+	text, _ = keyboard.ReadString('\n')
+	text = strings.ToLower(strings.TrimSpace(text))
+
+	if text == options[0] {
+		fmt.Println("Option:", options[0])
+		fmt.Print("Add thing to do => ")
+		newItem, _ := keyboard.ReadString('\n')
+		newItem = strings.TrimSpace(newItem)
+		if newItem != "" {
+			items_todo = append(items_todo, newItem)
+		}
+		fmt.Println("Updated To Do List >>", items_todo)
+	} else if text == options[1] {
+		fmt.Println("Option:", options[1])
+		// naive “complete last item” keeping your simple flow
+		if len(items_todo) > 0 {
+			items_todo = items_todo[:len(items_todo)-1]
+		}
+		fmt.Println("Updated To Do List >>", items_todo)
+	} else if text == options[2] {
+		fmt.Println("Option:", options[2])
+		fmt.Println("To Do List >>", items_todo)
+	} else {
+		fmt.Println("Unknown option.")
 	}
+
 }
